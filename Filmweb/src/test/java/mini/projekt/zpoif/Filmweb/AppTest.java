6c9f6524c7e46821ac9ca0ca21e1aba76154cb08
ssafiejko <100367820+ssafiejko@users.noreply.github.com>
2023-02-09 00:19:02 +0100

    : )

---
+++ Filmweb/src/test/java/mini/projekt/zpoif/Filmweb/AppTest.java
@@ -0,0 +1,20 @@
+package mini.projekt.zpoif.Filmweb;
+
+import static org.junizt.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
